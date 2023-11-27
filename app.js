04ba54f3f44cd258697558dbb168f7456946d33a
Josy-coder <josekecha@keemail.me>
2023-11-27 17:04:54 +0200

    added a create a task form and updated the task functionalities such as move from not started to started to completed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskList = document.getElementById('tasks');
+    const taskForm = document.getElementById('task-form');
+    let currentTab = 'not-started';
   
     // Load tasks from local storage
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -11,64 +13,61 @@ document.addEventListener('DOMContentLoaded', () => {
     function renderTasks() {
       taskList.innerHTML = '';
       tasks.forEach((task, index) => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-          <span class="task-name" contenteditable="true" onblur="editTask(${index})">${task.name}</span>
-          <span class="subtasks">${task.subtasks ? `(${task.subtasks} subtasks)` : ''}</span>
-          <span class="priority">Priority: 
-            <select onchange="editPriority(${index}, this.value)">
-              <option value="Low" ${task.priority === 'Low' ? 'selected' : ''}>Low</option>
-              <option value="Medium" ${task.priority === 'Medium' ? 'selected' : ''}>Medium</option>
-              <option value="High" ${task.priority === 'High' ? 'selected' : ''}>High</option>
-            </select>
-          </span>
-          <span class="deadline">Deadline: 
-            <input type="date" value="${task.deadline || ''}" onchange="editDeadline(${index}, this.value)">
-          </span>
-          <button class="delete-btn" onclick="deleteTask(${index})">Delete</button>
-        `;
-        listItem.draggable = true;
-        listItem.addEventListener('dragstart', (e) => handleDragStart(e, index));
-        listItem.addEventListener('dragover', (e) => handleDragOver(e));
-        listItem.addEventListener('drop', (e) => handleDrop(e, index));
-        taskList.appendChild(listItem);
+        if (
+          (currentTab === 'not-started' && task.status === 'Not Started') ||
+          (currentTab === 'started' && task.status === 'Started') ||
+          (currentTab === 'completed' && task.status === 'Completed')
+        ) {
+          const listItem = document.createElement('li');
+          listItem.innerHTML = `
+            <span class="task-name" contenteditable="true" onblur="editTaskName(${index})">${task.name}</span>
+            <span class="subtasks">${task.subtasks ? `(${task.subtasks} subtasks)` : ''}</span>
+            <span class="priority">Priority: ${task.priority}</span>
+            <span class="deadline">Deadline: ${task.deadline || 'Not set'}</span>
+            <span class="status">Status: ${task.status}</span>
+            <button class="complete-btn" onclick="toggleTaskStatus(${index})">${
+              task.status === 'Not Started' ? 'Start' : 'Complete'
+            }</button>
+            <button class="delete-btn" onclick="deleteTask(${index})">Delete</button>
+          `;
+          listItem.draggable = true;
+          listItem.addEventListener('dragstart', (e) => handleDragStart(e, index));
+          listItem.addEventListener('dragover', (e) => handleDragOver(e));
+          listItem.addEventListener('drop', (e) => handleDrop(e, index));
+          taskList.appendChild(listItem);
+        }
       });
       updateLocalStorage();
     }
   
     // Function to add a new task
     window.addTask = function () {
-      const newTaskInput = document.getElementById('new-task');
-      const newTaskName = newTaskInput.value.trim();
+      const newTaskName = document.getElementById('task-name').value.trim();
       if (newTaskName) {
         const newTask = {
           name: newTaskName,
-          subtasks: 0,
-          priority: 'Medium',
-          deadline: ''
+          subtasks: document.getElementById('subtasks').value.trim(),
+          priority: document.getElementById('priority').value,
+          deadline: document.getElementById('deadline').value,
+          status: 'Not Started',
         };
         tasks.push(newTask);
         renderTasks();
-        newTaskInput.value = '';
+        taskForm.reset();
       }
     };
   
     // Function to edit a task name
-    window.editTask = function (index) {
+    window.editTaskName = function (index) {
       const editedName = document.getElementsByClassName('task-name')[index].innerText.trim();
       tasks[index].name = editedName;
       renderTasks();
     };
   
-    // Function to edit priority
-    window.editPriority = function (index, priority) {
-      tasks[index].priority = priority;
-      renderTasks();
-    };
-  
-    // Function to edit deadline
-    window.editDeadline = function (index, deadline) {
-      tasks[index].deadline = deadline;
+    // Function to toggle task status (Start/Complete)
+    window.toggleTaskStatus = function (index) {
+      const task = tasks[index];
+      task.status = task.status === 'Not Started' ? 'Started' : 'Completed';
       renderTasks();
     };
   
@@ -90,15 +89,29 @@ document.addEventListener('DOMContentLoaded', () => {
   
     function handleDragOver(e) {
       e.preventDefault();
+      const draggedOverItem = e.target.closest('li');
+      if (draggedOverItem) {
+        draggedOverItem.classList.add('dragged-over');
+      }
     }
   
     function handleDrop(e, dropIndex) {
       e.preventDefault();
+      const draggedOverItem = e.target.closest('li');
+      if (draggedOverItem) {
+        draggedOverItem.classList.remove('dragged-over');
+      }
       const draggedTask = tasks[dragIndex];
       tasks.splice(dragIndex, 1);
       tasks.splice(dropIndex, 0, draggedTask);
       renderTasks();
     }
+  
+    // Function to show tasks based on tab selection
+    window.showTab = function (tab) {
+      currentTab = tab;
+      renderTasks();
+    };
   
     // Function to update local storage
     function updateLocalStorage() {
